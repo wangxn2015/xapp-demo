2414ff8eed6e73c1851cea0be9d0c894c125001b
wangxn2015 <wangxn_007@163.com>
2023-05-17 08:26:27 +0800

    update comments

---
+++ aa/readme.txt
@@ -0,0 +1,3 @@
+Please do not use the pb.go file in aa directory.
+Please do not use the make gen command in Makefile.
+
--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,5 @@
+//本示例xapp-demo用于演示如何制作一个xapp，用以访问KPM xapp北向接口提供的数据
+
 package main
 
 import (
@@ -8,7 +10,7 @@ import (
 )
 
 const (
-	defaultAddress = "192.168.127.113:6789"
+	defaultAddress = "192.168.127.113:6789" //将要连接的， KPM xapp的北向接口
 )
 const (
 	configDir  = ".onos"
@@ -48,7 +50,7 @@ func main() {
 	keyPath := flag.String("keyPath", "certs/tls.key", "path to client private key")
 	certPath := flag.String("certPath", "certs/tls.crt", "path to client certificate")
 	kpimonEndpoint := flag.String("kpimonEndpoint", defaultAddress, "kpimon service endpoint")
-	noTLSFlag := flag.Bool(noTLSKey, false, "no TLS flag -- true")
+	noTLSFlag := flag.Bool(noTLSKey, false, "no TLS flag -- true or false")
 	//ricActionID := flag.Int("ricActionID", 10, "RIC Action ID in E2 message")
 	//configPath := flag.String("configPath", "/etc/onos/config/config.json", "path to config.json file")
 	//grpcPort := flag.Int("grpcPort", 5150, "grpc Port number")
@@ -57,6 +59,7 @@ func main() {
 
 	flag.Parse()
 
+	// 用于检测证书，无实际功能
 	_, err := certs.HandleCertPaths(*caPath, *keyPath, *certPath, true)
 	if err != nil {
 		log.Fatal(err)
